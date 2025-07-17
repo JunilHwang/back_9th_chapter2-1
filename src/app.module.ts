0fd3791f348c829a10fd19b2034b5231e24c3e52
junilhwang <junil.h@kakao.com>
2025-07-18 00:13:11 +0900

    feat: app.module에서 나머지 module을 불러오도록 함

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,24 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { MockDatabaseModule } from './database/mock/database.mock.module';
 import { AppController } from './app.controller';
+import {
+  BalanceModule,
+  CouponModule,
+  OrderModule,
+  ProductModule,
+} from './modules';
 
 const DatabaseModuleForEnv =
   process.env.NODE_ENV === 'test' ? MockDatabaseModule : DatabaseModule;
 
 @Module({
-  imports: [DatabaseModuleForEnv],
+  imports: [
+    DatabaseModuleForEnv,
+    BalanceModule,
+    ProductModule,
+    CouponModule,
+    OrderModule,
+  ],
   controllers: [AppController],
   providers: [],
 })
