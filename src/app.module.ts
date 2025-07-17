762a719d87ddbcc71445d4cf68dd2aaddf0c20d7
junilhwang <junil.h@kakao.com>
2025-07-17 23:05:57 +0900

    chore: Mock DB 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
+import { MockDatabaseModule } from './database/mock/database.mock.module';
+import { AppController } from './app.controller';
+
+const DatabaseModuleForEnv =
+  process.env.NODE_ENV === 'test' ? MockDatabaseModule : DatabaseModule;
 
 @Module({
-  imports: [DatabaseModule],
-  controllers: [],
+  imports: [DatabaseModuleForEnv],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
